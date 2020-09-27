92f6136136e20fa1e909f770cbf7261332a8bfb8
Jaine Senna <39140368+jainesenna@users.noreply.github.com>
2020-09-27 16:00:28 -0300

    Update meupid

--- meupid.c
+++ meupid.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(){
+int main()
+{
   printf("Eu sou o processo %d. Meu pai é o %d. O ID do grupo de processo é o %d. \n" ,getpid(), getppid(), getpgrp());
   sleep(120);
   exit(0);
 };
+
+/*
+  Para compilar este programa salvo como "meupid.c"
+  $ gcc meupid.c -o meupid
+  $ ./meupid
+*/
 
