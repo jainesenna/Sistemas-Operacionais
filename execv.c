f297cf680ff535688a61265efa7eef9a2df865e0
Jaine Senna <jainesantossenna191@gmail.com>
2020-09-27 16:04:32 -0300

    Update execv

--- execv.c
+++ execv.c
@@ -2,10 +2,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(){
+int main()
+{
   char *cmd[] = { "cat", "/etc/passwd", (char *)0};
   printf("Vou chamar o programa cat para ler o conteudo de /etc/passwd.\n");
   execv("/bin/cat",cmd);
   printf("Esta mensagem não será impressa.\n");
   exit(0);
 }
+
+/*
+  Para compilar este programa salvo como "execv.c"
+  $ gcc execv.c -o execv
+  $ ./execv
+*/
