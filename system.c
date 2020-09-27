f94d2b4749f12d26ed35d4aa909c7df5d8c903a5
Jaine Senna <jainesantossenna191@gmail.com>
2020-09-27 16:12:47 -0300

    Update system

--- system.c
+++ system.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(){
+int main()
+{
   printf("Vou chamar o programa cat para ler o conteúdo de /etc/issue.\n");
   system("/bin/cat /etc/issue");
   printf("Esta mensagem será impressa.\n");
   exit(0);      
 }
+
+/*
+  Para compilar este programa salvo como "system.c"
+  $ gcc system.c -o system
+  $ ./system
+*/
